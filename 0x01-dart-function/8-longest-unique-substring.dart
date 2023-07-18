f670493211ad6b38aea108b033d0d21dde74cdab
Elodie Riou <elodie.riou@hotmail.com>
2023-07-18 11:55:27 +0200

    8. Longest Unique Substring

--- 0x01-dart-function/8-longest-unique-substring.dart
+++ 0x01-dart-function/8-longest-unique-substring.dart
@@ -1,6 +1,7 @@
 String longestUniqueSubstring(String str) {
   Map<String, int> counter = {};
   String substring = '';
+  String longestSubstring = '';
 
   for (int i = 0; i < str.length; i++) {
     counter[str[i]] = (counter[str[i]] ?? 0) + 1;
@@ -10,6 +11,8 @@ String longestUniqueSubstring(String str) {
       substring = substring.substring(startIndex + 1);
     }
     substring += str[i];
+
+    if (substring.length > longestSubstring.length) longestSubstring = substring;
   }
-  return substring;
+  return longestSubstring;
 }

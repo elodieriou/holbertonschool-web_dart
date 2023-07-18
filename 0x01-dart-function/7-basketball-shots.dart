6f3b83e2b6237b06b792978381262c2dfcd86ad9
Elodie Riou <elodie.riou@hotmail.com>
2023-07-18 10:15:16 +0200

    7. Basketball Shots

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -2,6 +2,9 @@ int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
   int teamAPoints = calculatePointsByTeam(teamA);
   int teamBPoints = calculatePointsByTeam(teamB);
 
+  print('team A $teamAPoints');
+  print('team B $teamBPoints');
+
   if (teamAPoints > teamBPoints) return 1;
   else if (teamAPoints < teamBPoints) return 2;
   else return 0;
@@ -9,8 +12,8 @@ int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
 
 int calculatePointsByTeam(Map<String, int> team) {
   int freeThrows = team['Free throws'] ?? 0;
-  int twoPointers = team['2 pointer'] ?? 0;
-  int threePointers = team['3 pointer'] ?? 0;
+  int twoPointers = team['2 pointers'] ?? 0;
+  int threePointers = team['3 pointers'] ?? 0;
 
   return (freeThrows * 1) + (twoPointers * 2) + (threePointers * 3);
 }

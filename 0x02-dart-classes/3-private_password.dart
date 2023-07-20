20acf34390bdd80651a1c0e68a89b5ee3f3c8481
Elodie Riou <elodie.riou@hotmail.com>
2023-07-20 09:37:58 +0200

    3. Private Password

--- 0x02-dart-classes/3-private_password.dart
+++ 0x02-dart-classes/3-private_password.dart
@@ -1,23 +1,23 @@
 class Password {
-  String password = '';
+  String _password = '';
 
   bool isValid() {
 
     // Check password length
-    if (password.length < 8 || password.length > 16) return false;
+    if (_password.length < 8 || _password.length > 16) return false;
 
     // Check for uppercase and lowercase letters
     bool hasUppercase = false;
     bool hasLowercase = false;
-    for (int i = 0; i < password.length; i++) {
-      if (password[i].toUpperCase() != password[i]) hasLowercase = true;
-      if (password[i].toLowerCase() != password[i]) hasUppercase = true;
+    for (int i = 0; i < _password.length; i++) {
+      if (_password[i].toUpperCase() != _password[i]) hasLowercase = true;
+      if (_password[i].toLowerCase() != _password[i]) hasUppercase = true;
     }
 
     // Check for numbers
     bool hasNumber = false;
-    for (int i = 0; i < password.length; i++) {
-      if (int.tryParse(password[i]) != null) hasNumber = true;
+    for (int i = 0; i < _password.length; i++) {
+      if (int.tryParse(_password[i]) != null) hasNumber = true;
     }
 
     return hasUppercase && hasLowercase && hasNumber;
@@ -25,6 +25,6 @@ class Password {
 
   @override
   String toString() {
-    return 'Your Password is: ${password}';
+    return 'Your Password is: ${_password}';
   }
 }

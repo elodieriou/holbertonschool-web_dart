0ae12eaacb2b909dd513e53024c0bb20cc7eb530
Elodie Riou <elodie.riou@hotmail.com>
2023-07-24 09:38:29 +0200

    0. Users Count

--- 0x03-asynchronous/0-users_count.dart
+++ 0x03-asynchronous/0-users_count.dart
@@ -1,9 +1,6 @@
 import 'dart:async';
 
 Future<void> usersCount() async {
-  // Simulate fetching data from an API
   int count = await fetchUsersCount();
-
-  // Print the number of users
   print(count);
 }

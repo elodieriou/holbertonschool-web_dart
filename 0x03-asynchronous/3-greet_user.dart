882e8b05015f1127776588fdd90745736373588d
Elodie Riou <elodie.riou@hotmail.com>
2023-07-24 09:40:01 +0200

    3. Greet User

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+import 'dart:convert';
+
+Future<String> greetUser() async {
+  try {
+    String userData = await fetchUserData();
+
+    Map<String, dynamic> userDataMap = jsonDecode(userData);
+    String username = userDataMap['username'];
+
+    return 'Hello $username';
+  } catch (error) {
+    return 'error caught: $error';
+  }
+}
+
+Future<String> loginUser() async {
+  try {
+    bool isValidUser = await checkCredentials();
+    print('There is a user: $isValidUser');
+
+    return (isValidUser) ? await greetUser() : 'Wrong credentials';
+  } catch (error) {
+    return 'error caught: $error';
+  }
+}

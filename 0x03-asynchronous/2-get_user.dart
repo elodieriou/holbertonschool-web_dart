6b550e029da6e3cc0ccbce29e955b41cdcc18108
Elodie Riou <elodie.riou@hotmail.com>
2023-07-25 08:46:07 +0200

    fix error main

--- 0x03-asynchronous/2-get_user.dart
+++ 0x03-asynchronous/2-get_user.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 
 Future<void> getUser() async {
   try {
-    String userData = await fetchUserData();
+    String userData = await fetchUser();
     print(userData.toString());
   } catch (error) {
     print('error caught: $error');
